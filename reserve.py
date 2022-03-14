82c170bc289c8ebe16ce0c7b178b3f24722e6e82
kghandhi <kira.ghandhi@datadoghq.com>
2022-03-14 18:58:35 -0400

    Adding loops and better success detection

--- reserve.py
+++ reserve.py
@@ -1,19 +1,21 @@
-import subprocess
-from pathlib import Path
-
+import os
 import selenium
 import selenium.webdriver
-from selenium.webdriver.common.by import By
+import subprocess
 import sys
 import time
+
+from datetime import date
 from datetime import datetime
 from datetime import timedelta
-from datetime import date
-import sys
-ACCOUNT_INFO=""
+from dotenv import load_dotenv
+from pathlib import Path
+from selenium.webdriver.common.by import By
 
+# This should be in local storage on the browser when you login
+# Application->Local Storage->https://recreation.gov->recaccount (copy full json)
+ACCOUNT_INFO=os.getenv("ACCOUNT_INFO")
 REC_URL = "https://www.recreation.gov/ticket/300017/ticket/3020"
-
 
 def book_rainforest(tomorrow=False):
     options = selenium.webdriver.chrome.options.Options()
@@ -38,8 +40,6 @@ def book_rainforest(tomorrow=False):
 
     driver.get(REC_URL)
 
-    # TODO: log in to recreation.gov, and set account_info equal to value associated w/ recaccount key in Local Storage.
-    # Store it as a string. It should start with {"access token": ....
     script = "localStorage.setItem('recaccount', '{}');".format(ACCOUNT_INFO)
 
     driver.execute_script(script)
@@ -50,21 +50,34 @@ def book_rainforest(tomorrow=False):
     tomorrow_date = now + timedelta(days=1)
     if tomorrow:
         curr_hour = now.hour
+        # Assumes tickets are released at 8am and 11am in same timezone
         h = 11 if abs(11-curr_hour) < abs(8-curr_hour) else 8
         print("closer to:", h)
-        wait_until = datetime(tomorrow_date.year, tomorrow_date.month, tomorrow_date.day, h)
-        print(wait_until)
+        wait_until = datetime(now.year, now.month, now.day, h)
+        print("waiting until", wait_until)
+        loop = 0
         while datetime.now() < wait_until:
+            if loop % 15 == 0:
+                print("still waiting...")
             time.sleep(0.1)
+            loop += 1
 
     print("let's gooooo")
 
     reserved = reload_and_reserve(driver, tomorrow_date, tomorrow)
-    if tomorrow and not reserved:
-        while not reserved:
-            time.sleep(1)
-            print("Trying again")
-            reserved = reload_and_reserve(driver, tomorrow_date, tomorrow)
+    if not reserved:
+        if tomorrow:
+            while not reserved:
+                time.sleep(1)
+                print("Trying again")
+                reserved = reload_and_reserve(driver, tomorrow_date, tomorrow)
+        else:
+            while True:
+                time.sleep(1)
+    else:
+        while True:
+            print("BOOK IT!!")
+            time.sleep(10)
 
 
 def reload_and_reserve(driver, tomorrow_date, tomorrow):
@@ -72,13 +85,10 @@ def reload_and_reserve(driver, tomorrow_date, tomorrow):
 
     input_element = driver.find_element(By.XPATH, "//input[@name='tourCalendarWithKey']")
 
-    # TODO: Set the date in MM/DD/YYYY format
-    desired_date = tomorrow_date.strftime("%m/%d/%Y") if tomorrow else "03/31/2022"
+    desired_date = tomorrow_date.strftime("%m/%d/%Y") if tomorrow else "04/10/2022"
+    print("booking for", desired_date)
     input_element.send_keys(desired_date)
 
-    time_slot = []
-    while len(time_slot) == 0:
-        time_slot = driver.find_elements(By.XPATH, "//input[@type='radio']")
     pill = []
     while len(pill) == 0:
         pill = driver.find_elements(By.XPATH, "//div[@class='ti-radio-pill']")
@@ -86,28 +96,35 @@ def reload_and_reserve(driver, tomorrow_date, tomorrow):
 
 
     for slot in pill:
-       print("reserving!")
-       action = selenium.webdriver.common.action_chains.ActionChains(driver)
-       action.move_to_element(slot)
-       action.click()
-       action.perform()
-       reserve_button = driver.find_element(By.XPATH, '//*[@id="page-content"]/main/div[2]/div/div[1]/div[1]/div/div[3]/div[2]/button')
-       reserve_button.click()
+        print("reserving!")
+        action = selenium.webdriver.common.action_chains.ActionChains(driver)
+        action.move_to_element(slot)
+        action.click()
+        action.perform()
+        if 'reservation' in driver.current_url:
+           break
+        try:
+           reserve_button = driver.find_element(By.XPATH, '//*[@id="page-content"]/main/div[2]/div/div[1]/div[1]/div/div[3]/div[2]/button')
+           reserve_button.click()
+        except Exception:
+            continue
 
-    cart_quantity = driver.find_elements(By.XPATH, "//span[@class='cart-quantity']")
-    print(len(cart_quantity))
-    if len(cart_quantity) > 0:
-        print("you have", cart_quantity[0].text, "reservations")
-        return True
+
+    maybe_cart = driver.current_url
+    if 'reservation' in maybe_cart:
+        cart_quantity = driver.find_elements(By.XPATH, "//span[@class='cart-quantity']")
+        if len(cart_quantity) > 0:
+            print("you have", cart_quantity[0].text, "reservations")
+            return True
+        else:
+            print("you have no reservations")
+            return False
     else:
         print("you have no reservations")
         return False
 
-
-
-
-
 if __name__ == "__main__":
+    load_dotenv()
+    ACCOUNT_INFO=os.getenv("ACCOUNT_INFO")
+
     book_rainforest(len(sys.argv) > 1)
-
-
